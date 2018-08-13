63916905b7c62329ce81592e1bea86580e2c83e3
haobingfu <haobingfu@frogsing.com>
2018-08-13 09:35:25 +0800

    modify commit

--- parent/master/src/main/java/com/inspire/MasterApplication.java
+++ parent/master/src/main/java/com/inspire/MasterApplication.java
@@ -1,5 +1,6 @@
 package com.inspire;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Date;
 
 @SpringBootApplication
+@Slf4j
 public class MasterApplication {
 
 	public static void main(String[] args) {
 		try{
 			SpringApplication.run(MasterApplication.class, args);
-			System.out.println("Application：应用程序正常启动..."+new Date());
+			log.info("Application：应用程序正常启动..."+new Date());
 		}catch(Exception e){
 			if(StringUtils.isNotBlank(e.getMessage())){
 				e.printStackTrace();
--- parent/master/src/main/java/com/inspire/sys/AdminController.java
+++ parent/master/src/main/java/com/inspire/sys/AdminController.java
@@ -1,5 +1,6 @@
 package com.inspire.sys;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping(value = "/sys")
+@Slf4j
 public class AdminController {
-
     @RequestMapping(value = "index.shtml")
     public String index(Model model){
         model.addAttribute("msg","您好，springboot");

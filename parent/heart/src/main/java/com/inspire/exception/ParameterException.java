e8f1a5b11bce96c51af478c54a3584057f120631
haobingfu <haobingfu@frogsing.com>
2018-08-16 09:08:55 +0800

    modify commit

---
+++ parent/heart/src/main/java/com/inspire/exception/ParameterException.java
@@ -0,0 +1,32 @@
+package com.inspire.exception;
+
+
+import com.inspire.webUtils.MsgUtils;
+
+/**
+ * Description:
+ *
+ * @author <a href="mailto:service@frogsing.com">Sandy</a>
+ * @since version1.0
+ */
+public class ParameterException extends RuntimeException {
+
+    private static final long serialVersionUID = 3583566093089790852L;
+
+    public ParameterException() {
+        super();
+    }
+
+    public ParameterException(String message) {
+        super(MsgUtils.getMsg(message));
+    }
+
+    public ParameterException(Throwable cause) {
+        super(cause);
+    }
+
+    public ParameterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
